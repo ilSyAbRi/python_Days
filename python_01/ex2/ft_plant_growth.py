cc3b4b3de0311668725d4e18c0c964b7c12d7d09
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-11 18:35:03 +0100

    do the ex

--- python_01/ex2/ft_plant_growth.py
+++ python_01/ex2/ft_plant_growth.py
@@ -7,8 +7,21 @@ class Plant:
     def grow(self):
         self.height += 1
 
-    def age(self):
+    def grow_older(self):
         self.age += 1
 
     def get_info(self):
         print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+rose = Plant("rose", 25, 30)
+print("=== Day 1 ===")
+day = 1
+rose.get_info()
+while day < 7:
+    rose.grow()
+    rose.grow_older()
+    day += 1
+print(f"=== Day {day} ===")
+rose.get_info()
+print("Growth this week: +6cm")
