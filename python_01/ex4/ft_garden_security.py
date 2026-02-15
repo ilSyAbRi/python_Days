ffd47293beb55cba7567b1b5a13c6232cb8d6bed
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-15 13:16:28 +0000

    learn more about privat variable and know that i can t acess to it from input i mean when i do python3 -i file.py

--- python_01/ex4/ft_garden_security.py
+++ python_01/ex4/ft_garden_security.py
@@ -1,25 +1,40 @@
-class   SecurePlant:
-    def __init__(self,name):
+class SecurePlant:
+    def __init__(self, name):
         self.name = name
         self.__height = 0
         self.__age = 0
+        print("Plant created:", self.name)
 
-    def set_height(self,height):
+    def set_height(self, height):
         if (height < 0):
-            return False
+            print("\nInvalid operation attempted: height " +
+                  str(height) + "cm [REJECTED]")
+            print("Security: Negative height rejected")
         else:
             self.__height = height
-            return True
+            print("Height updated:" + str(self.__height) + "cm [OK]")
 
     def set_age(self, age):
         if (age < 0):
-            return False
+            print("\nInvalid operation attempted: age", age, "[REJECTED]")
+            print("Security: Negative age rejected")
         else:
             self.__age = age
-            return  True
+            print("Age updated:", self.__age, "days [OK]")
 
     def get_height(self):
-        return  self.__height
+        return self.__height
 
     def get_age(self):
-        return  self.__age
+        return self.__age
+
+
+print("=== Garden Security System ===")
+plant = SecurePlant("Rose")
+plant.set_height(25)
+plant.set_age(30)
+plant.set_height(-5)
+
+print("\nCurrent plant: " + plant.name + " ("
+      + str(plant.get_height()) + "cm, "
+      + str(plant.get_age()) + " days)")
