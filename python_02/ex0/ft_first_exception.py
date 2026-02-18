40e94511c6e44ca21077c6a2db5c7fed5dfcd3bd
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-18 17:57:35 +0000

    first step

--- python_02/ex0/ft_first_exception.py
+++ python_02/ex0/ft_first_exception.py
@@ -1,1 +1,18 @@
 def check_temperature(temp_str):
+    """
+    Converts a string to a number
+    and checks if it is a valid plant temperature (0–40°C).
+    """
+    print(f"Testing temperature: {temp}")
+    try:
+        temp = int(temp_str)
+        if (temp > 40)
+            print(f"Error: {temp}°C is too hot for plants (max 40°C)")
+        elif (temp < 0)
+            print(f"Error: {temp}°C is too cold for plants (min 0°C)"
+        else
+            print(f"Temperature {temp}°C is perfect for plants!")
+
+        print("All tests completed - program didn't crash!")
+    except ValueError:
+        print(f"Error: '{temp}' is not a valid number")
