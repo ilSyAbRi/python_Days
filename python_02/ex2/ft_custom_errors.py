5bb26fc2fbe4aa1b93e6a9ba46a1acbd64256b89
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-19 23:51:53 +0000

    start the ex

---
+++ python_02/ex2/ft_custom_errors.py
@@ -0,0 +1,16 @@
+class GardenError(Exception):
+    pass
+
+
+class PlantError(GardenError):
+    pass
+
+
+class WaterError(GardenError):
+    pass
+
+def check_plant()
+    raise PlantError("The tomato plant is wilting!")
+def check_water()
+    raise WaterError("Not enough water in the tank!")
+ft_custom_errors.py
