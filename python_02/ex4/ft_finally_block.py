5aabe4c54b8e1690aa6a0e34b2ed4c7c2d763639
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-20 17:38:07 +0000

    finish this ex it s simple i just learn that raise stop the function and return the error imediatly to the caller

--- python_02/ex4/ft_finally_block.py
+++ python_02/ex4/ft_finally_block.py
@@ -8,34 +8,37 @@ def check_plant_health(plant_name, water_level, sunlight_hours):
     if sunlight_hours < 2:
         raise ValueError(f"Sunlight hours {sunlight_hours} is too low (min 2)")
     if sunlight_hours > 12:
-        raise ValueError(f"Sunlight hours {sunlight_hours} is too high (max 12)")
+        raise ValueError(
+                f"Sunlight hours {sunlight_hours} is too high (max 12)"
+                )
     return f"Plant '{plant_name}' is healthy!"
+
 
 def test_plant_checks():
     print("=== Garden Plant Health Checker ===")
-    print("Testing good values...")
+    print("\nTesting good values...")
     try:
         print(check_plant_health("tomato", 5, 6))
     except ValueError as e:
         print("Error:", e)
-    print("Testing empty plant name...")
+    print("\nTesting empty plant name...")
     try:
         print(check_plant_health("", 5, 6))
     except ValueError as e:
         print("Error:", e)
 
-    print("Testing bad water level...")
+    print("\nTesting bad water level...")
     try:
         print(check_plant_health("tomato", 15, 6))
     except ValueError as e:
         print("Error:", e)
 
-    print("Testing bad sunlight hours...")
+    print("\nTesting bad sunlight hours...")
     try:
         print(check_plant_health("tomato", 5, 0))
     except ValueError as e:
         print("Error:", e)
-    print("All error raising tests completed!")
+    print("\nAll error raising tests completed!")
 
 
 if __name__ == "__main__":
