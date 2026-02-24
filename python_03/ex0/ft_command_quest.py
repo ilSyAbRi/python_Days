283b63af96b8a5f683e52e79c8006c91a3dae9b8
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-24 15:19:34 +0000

    first ex

--- python_03/ex0/ft_command_quest.py
+++ python_03/ex0/ft_command_quest.py
@@ -1,1 +1,16 @@
+import sys
 
+print("=== Command Quest ===")
+if len(sys.argv) == 1:
+    print("No arguments provided!")
+    print("Program name:", sys.argv[0])
+    print("Total arguments:", len(sys.argv))
+else:
+    print("Program name:",sys.argv[0])
+    print("Arguments received:", len(sys.argv) -1)
+
+    i = 1
+    while i < len(sys.argv):
+        print("Argument",i,":",sys.argv[i])
+        i += 1
+    print("Total arguments:", len(sys.argv))
