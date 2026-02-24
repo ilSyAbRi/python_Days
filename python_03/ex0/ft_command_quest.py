290fa7df8f29a90376c8f3511d9fe2bebda2d94b
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-24 15:20:56 +0000

    do flake

--- python_03/ex0/ft_command_quest.py
+++ python_03/ex0/ft_command_quest.py
@@ -6,11 +6,11 @@ if len(sys.argv) == 1:
     print("Program name:", sys.argv[0])
     print("Total arguments:", len(sys.argv))
 else:
-    print("Program name:",sys.argv[0])
-    print("Arguments received:", len(sys.argv) -1)
+    print("Program name:", sys.argv[0])
+    print("Arguments received:", len(sys.argv)-1)
 
     i = 1
     while i < len(sys.argv):
-        print("Argument",i,":",sys.argv[i])
+        print("Argument", i, ":", sys.argv[i])
         i += 1
     print("Total arguments:", len(sys.argv))
