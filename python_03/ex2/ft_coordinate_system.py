ffb8b89d7651b0c1b77cad19c9929ce7ea5b937f
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-24 23:11:59 +0000

    create position and calculate distance

--- python_03/ex2/ft_coordinate_system.py
+++ python_03/ex2/ft_coordinate_system.py
@@ -1,1 +1,14 @@
-start the ex
+import math
+
+def coordinate_system():
+    print("=== Game Coordinate System ===")
+
+    position = (10, 20, 5)
+    print("Position created:" position)
+
+    origin = (0, 0, 0)
+
+    x1, y1, z1 = origin
+    x2, y2, z2 = position
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+    print(f"Distance between {origin} and {position}: {round(distance, 2)}")
