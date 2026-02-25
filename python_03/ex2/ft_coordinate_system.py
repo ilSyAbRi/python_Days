8c1b7b54d9a334ff21cc6bc53b367afe75fd0f12
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-25 04:37:05 +0000

    finish the ex ?

--- python_03/ex2/ft_coordinate_system.py
+++ python_03/ex2/ft_coordinate_system.py
@@ -1,7 +1,8 @@
 import math
+import sys
 
 
-def coordinate_system():
+def step1():
     print("=== Game Coordinate System ===")
 
     position = (10, 20, 5)
@@ -41,4 +42,44 @@ def coordinate_system():
     print(f"Coordinates: X={x}, Y={y}, Z={z}")
 
 
-coordinate_system()
+def step2():
+
+    if len(sys.argv) != 3:
+        print("\nUsage: python3 ft_coordinate_system.py\
+start_x,start_y,start_z target_x,target_y,target_z")
+        return
+
+    start_str = sys.argv[1]
+    target_str = sys.argv[2]
+
+    try:
+        s_parts = start_str.split(",")
+        t_parts = target_str.split(",")
+
+        start_position = (int(s_parts[0]), int(s_parts[1]), int(s_parts[2]))
+        print("\nPosition created:", start_position)
+
+        print(f'Parsing coordinates: "{target_str}"')
+        parsed_position = (int(t_parts[0]), int(t_parts[1]), int(t_parts[2]))
+        print("Parsed position:", parsed_position)
+
+        x1, y1, z1 = start_position
+        x2, y2, z2 = parsed_position
+        distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+        print(f"\nDistance between {start_position}\
+and {parsed_position}: {round(distance, 2)}")
+
+    except Exception as e:
+        print("\nError parsing coordinates:", e)
+        print("Error details - Type:", type(e).__name__, ", Args:", e.args)
+        return
+
+    print("\nUnpacking demonstration:")
+    x, y, z = parsed_position
+    print(f"Player at x={x}, y={y}, z={z}")
+    print(f"Coordinates: X={x}, Y={y}, Z={z}")
+
+
+if __name__ == "__main__":
+    step1()
+    step2()
