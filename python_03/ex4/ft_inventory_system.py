f8c787f50c5281af0e16501c76257dfa0c600ae7
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-26 21:16:43 +0000

    nested dictionary

--- python_03/ex4/ft_inventory_system.py
+++ python_03/ex4/ft_inventory_system.py
@@ -1,1 +1,16 @@
-start the ex
+
+alice_inventory = {
+    "sword": {"type": "weapon", "rarity": "rare", "quantity": 1, "value": 500},
+    "potion": {"type": "consumable", "rarity": "common", "quantity": 5, "value": 50},
+    "shield": {"type": "armor", "rarity": "uncommon", "quantity": 1, "value": 200},
+}
+
+bob_inventory = {
+    "potion": {"type": "consumable", "rarity": "common", "quantity": 0, "value": 50},
+    "dagger": {"type": "weapon", "rarity": "uncommon", "quantity": 1, "value": 150},
+}
+
+players = {
+    "Alice": alice_inventory,
+    "Bob": bob_inventory
+}
