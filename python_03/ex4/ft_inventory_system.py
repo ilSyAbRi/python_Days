79ce91b2bbff515daa8a6abb852eed7e09f2d784
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-27 00:40:19 +0000

    we work whit this data

--- python_03/ex4/ft_inventory_system.py
+++ python_03/ex4/ft_inventory_system.py
@@ -1,16 +1,42 @@
-
-alice_inventory = {
-    "sword": {"type": "weapon", "rarity": "rare", "quantity": 1, "value": 500},
-    "potion": {"type": "consumable", "rarity": "common", "quantity": 5, "value": 50},
-    "shield": {"type": "armor", "rarity": "uncommon", "quantity": 1, "value": 200},
-}
-
-bob_inventory = {
-    "potion": {"type": "consumable", "rarity": "common", "quantity": 0, "value": 50},
-    "dagger": {"type": "weapon", "rarity": "uncommon", "quantity": 1, "value": 150},
-}
-
-players = {
-    "Alice": alice_inventory,
-    "Bob": bob_inventory
+my_dic = 
+{
+    "players": {
+        "alice": {
+            "items": {
+                "pixel_sword": 1,
+                "code_bow": 1,
+                "health_byte": 1,
+                "quantum_ring": 3,
+            },
+            "total_value": 1875,
+            "item_count": 6,
+        },
+        "bob": {
+            "items": {"code_bow": 3, "pixel_sword": 2},
+            "total_value": 900,
+            "item_count": 5,
+        },
+        "charlie": {
+            "items": {"pixel_sword": 1, "code_bow": 1},
+            "total_value": 350,
+            "item_count": 2,
+        },
+        "diana": {
+            "items": {
+                "code_bow": 3,
+                "pixel_sword": 3,
+                "health_byte": 3,
+                "data_crystal": 3,
+            },
+            "total_value": 4125,
+            "item_count": 12,
+        },
+    },
+    "catalog": {
+        "pixel_sword": {"type": "weapon", "value": 150, "rarity": "common"},
+        "quantum_ring": {"type": "accessory", "value": 500, "rarity": "rare"},
+        "health_byte": {"type": "consumable", "value": 25, "rarity": "common"},
+        "data_crystal": {"type": "material", "value": 1000, "rarity": "legendary"},
+        "code_bow": {"type": "weapon", "value": 200, "rarity": "uncommon"},
+    },
 }
