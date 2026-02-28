224e2337ff2b912b171ad5f798dc08cbd008f754
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-28 07:01:46 +0000

    do flake and add name main

--- python_03/ex0/ft_command_quest.py
+++ python_03/ex0/ft_command_quest.py
@@ -1,16 +1,17 @@
 import sys
 
-print("=== Command Quest ===")
-if len(sys.argv) == 1:
-    print("No arguments provided!")
-    print("Program name:", sys.argv[0])
-    print("Total arguments:", len(sys.argv))
-else:
-    print("Program name:", sys.argv[0])
-    print("Arguments received:", len(sys.argv)-1)
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    if len(sys.argv) == 1:
+        print("No arguments provided!")
+        print("Program name:", sys.argv[0])
+        print("Total arguments:", len(sys.argv))
+    else:
+        print("Program name:", sys.argv[0])
+        print("Arguments received:", len(sys.argv)-1)
 
-    i = 1
-    while i < len(sys.argv):
-        print("Argument", i, ":", sys.argv[i])
-        i += 1
-    print("Total arguments:", len(sys.argv))
+        i = 1
+        while i < len(sys.argv):
+            print("Argument", i, ":", sys.argv[i])
+            i += 1
+        print("Total arguments:", len(sys.argv))
