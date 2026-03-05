ca0bdc6af0d3999bad29ab5b9fcc0c02d712d873
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-05 22:39:58 +0000

    finish the ex

--- python_04/ex1/ft_archive_creation.py
+++ python_04/ex1/ft_archive_creation.py
@@ -1,8 +1,21 @@
 if __name__ == "__main__":
-    print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ==="
-    print("Initializing new storage unit: new_discovery.txt")
-    file = open("new_discovery.txt","w")
-    file.write("{[}ENTRY 001{]} New quantum algorithm discovered\n
-                {[}ENTRY 002{]} Efficiency increased by 347%\n
-                {[}ENTRY 003{]} Archived by Data Archivist trainee")
+    print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===")
+
+    print("\nInitializing new storage unit: new_discovery.txt")
+
+    file = open("new_discovery.txt", "w")
+    print("Storage unit created successfully...")
+
+    print("\nInscribing preservation data...")
+    file.write("{[}ENTRY 001{]} New quantum algorithm discovered\n")
+    file.write("{[}ENTRY 002{]} Efficiency increased by 347%\n")
+    file.write("{[}ENTRY 003{]} Archived by Data Archivist trainee\n")
+
     file.close()
+
+    print("{[}ENTRY 001{]} New quantum algorithm discovered")
+    print("{[}ENTRY 002{]} Efficiency increased by 347%")
+    print("{[}ENTRY 003{]} Archived by Data Archivist trainee")
+
+    print("\nData inscription complete. Storage unit sealed.")
+    print("Archive 'new_discovery.txt' ready for long-term preservation.")
