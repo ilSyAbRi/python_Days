5c12027c52abaa902af5463a0e3eba96767e2f2a
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-05 22:24:54 +0000

    start see the ex

--- python_04/ex1/ft_archive_creation.py
+++ python_04/ex1/ft_archive_creation.py
@@ -1,1 +1,8 @@
-start the ex
+if __name__ == "__main__":
+    print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ==="
+    print("Initializing new storage unit: new_discovery.txt")
+    file = open("new_discovery.txt","w")
+    file.write("{[}ENTRY 001{]} New quantum algorithm discovered\n
+                {[}ENTRY 002{]} Efficiency increased by 347%\n
+                {[}ENTRY 003{]} Archived by Data Archivist trainee")
+    file.close()
