83256ee5b0725c7ef1fad9e5ac1dd444d87f057f
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-05 23:47:32 +0000

    do flake

--- python_04/ex2/ft_stream_management.py
+++ python_04/ex2/ft_stream_management.py
@@ -6,9 +6,11 @@ archivist_id = input("\nInput Stream active. Enter archivist ID: ")
 
 status_report = input("Input Stream active. Enter status report: ")
 
-print(f"\n{{[}}STANDARD{{]}} Archive status from {archivist_id}: {status_report}")
+print(f"\n{{[}}STANDARD{{]}} Archive status"
+      f"from {archivist_id}: {status_report}")
 
-sys.stderr.write("{[}ALERT{]} System diagnostic: Communication channels verified\n")
+sys.stderr.write("{[}ALERT{]} System diagnostic: "
+                 "Communication channels verified\n")
 
 print("{[}STANDARD{]} Data transmission complete")
 
