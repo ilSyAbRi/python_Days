c9cc2d36bcfd78b72533002b7e512b1e1cefd8ae
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-06 01:12:47 +0000

    do new line

--- python_04/ex0/ft_ancient_text.py
+++ python_04/ex0/ft_ancient_text.py
@@ -1,13 +1,13 @@
 if __name__ == "__main__":
     try:
         print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
-        print("Accessing Storage Vault: ancient_fragment.txt")
+        print("\nAccessing Storage Vault: ancient_fragment.txt")
         file = open("ancient_fragment.txt", "r")
         print("Connection established...")
-        print("RECOVERED DATA:")
+        print("\nRECOVERED DATA:")
         print(file.read())
         file.close()
-        print("Data recovery complete. Storage unit disconnected.")
+        print("\nData recovery complete. Storage unit disconnected.")
     except FileNotFoundError:
         print("ERROR: Storage vault not found. Run data generator first.")
     except PermissionError:
