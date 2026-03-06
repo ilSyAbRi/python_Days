41c0a2929f3cfd7a5567a49e36ddd15fc7b5a018
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-06 00:37:41 +0000

    handle error

--- python_04/ex0/ft_ancient_text.py
+++ python_04/ex0/ft_ancient_text.py
@@ -1,13 +1,14 @@
-try:
-    print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
-    print("Accessing Storage Vault: ancient_fragment.txt")
-    file = open("ancient_fragment.txt", "r")
-    print("Connection established...")
-    print("RECOVERED DATA:")
-    print(file.read())
-    file.close()
-    print("Data recovery complete. Storage unit disconnected.")
-except FileNotFoundError:
-    print("ERROR: Storage vault not found. Run data generator first.")
-except PermissionError:
-    print("oh shit PermissionError here we go again")
+if __name__ == "__main__":
+    try:
+        print("=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===")
+        print("Accessing Storage Vault: ancient_fragment.txt")
+        file = open("ancient_fragment.txt", "r")
+        print("Connection established...")
+        print("RECOVERED DATA:")
+        print(file.read())
+        file.close()
+        print("Data recovery complete. Storage unit disconnected.")
+    except FileNotFoundError:
+        print("ERROR: Storage vault not found. Run data generator first.")
+    except PermissionError:
+        print("oh shit PermissionError here we go again")
