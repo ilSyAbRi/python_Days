db866216646e8472ace31fa6fd81672cb903a754
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-06 07:04:51 +0000

    fix problem

--- python_04/ex3/ft_vault_security.py
+++ python_04/ex3/ft_vault_security.py
@@ -7,10 +7,12 @@ if __name__ == "__main__":
     try:
         with (
             open("classified_data.txt", "r") as vault1,
-            open("security_protocols.txt", "r") as vault2
+            open("security_protocols.txt", "w") as vault2
         ):
             content1 = vault1.read()
-            content2 = vault2.read()
+            content2 = "[CLASSIFIED] New security protocols archived\n"
+            vault2.write(content2)
+
             print("\nSECURE EXTRACTION:")
             print(content1)
             print("\nSECURE PRESERVATION:")
@@ -18,6 +20,6 @@ if __name__ == "__main__":
             print("Vault automatically sealed upon completion")
             print("\nAll vault operations completed with maximum security.")
     except FileNotFoundError:
-        print("\n{[}CLASSIFIED{]} Vault missing! Extraction failed\n")
+        print("\n[CLASSIFIED] Vault missing! Extraction failed\n")
     except PermissionError:
         print("\noh shit PermissionError here we go again")
