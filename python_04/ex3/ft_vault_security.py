e8b3f50865554fa7bc2cdbf0288c88c4751b53f7
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-06 00:57:21 +0000

    try to do the ex

--- python_04/ex3/ft_vault_security.py
+++ python_04/ex3/ft_vault_security.py
@@ -4,23 +4,17 @@ if __name__ == "__main__":
     print("\nInitiating secure vault access...")
     print("Vault connection established with failsafe protocols")
 
-    print("\nSECURE EXTRACTION:")
     try:
-        with open("classified_data.txt", "r") as vault:
-            content = vault.read()
-            print(content, end="")
+        with (
+        open("classified_data.txt", "r") as vault1,
+        open("standard_archive.txt", "r") as vault2
+        ):
+            content1 = vault1.read()
+            content2 = vault2.read()
+            print(content1)
+            print(content2)
+            print("Vault automatically sealed upon completion")
     except FileNotFoundError:
         print("{[}CLASSIFIED{]} Vault missing! Extraction failed\n")
-    except PermissionError:
-        print("oh shit pPermissionError here we go again")
-
-    print("\nSECURE PRESERVATION:")
-    try:
-        with open("new_security_protocols.txt", "w") as vault:
-            vault.write("{[}CLASSIFIED{]} New security protocols archived\n")
-            print("{[}CLASSIFIED{]} New security protocols archived")
-
-        print("Vault automatically sealed upon completion")
-        print("\nAll vault operations completed with maximum security.")
     except PermissionError:
         print("oh shit PermissionError here we go again")
