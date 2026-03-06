ad4ac7281eeaba845bea60715ecd1e4d28fb7c1f
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-06 00:26:23 +0000

    handle error

--- python_04/ex1/ft_archive_creation.py
+++ python_04/ex1/ft_archive_creation.py
@@ -3,15 +3,18 @@ if __name__ == "__main__":
 
     print("\nInitializing new storage unit: new_discovery.txt")
 
-    file = open("new_discovery.txt", "w")
-    print("Storage unit created successfully...")
+    try:
+        file = open("new_discovery.txt", "w")
+        print("Storage unit created successfully...")
 
-    print("\nInscribing preservation data...")
-    file.write("{[}ENTRY 001{]} New quantum algorithm discovered\n")
-    file.write("{[}ENTRY 002{]} Efficiency increased by 347%\n")
-    file.write("{[}ENTRY 003{]} Archived by Data Archivist trainee\n")
+        print("\nInscribing preservation data...")
+        file.write("{[}ENTRY 001{]} New quantum algorithm discovered\n")
+        file.write("{[}ENTRY 002{]} Efficiency increased by 347%\n")
+        file.write("{[}ENTRY 003{]} Archived by Data Archivist trainee\n")
 
-    file.close()
+        file.close()
+    except : PermissionError
+        print("oh shit PermissionError error here we go again")
 
     print("{[}ENTRY 001{]} New quantum algorithm discovered")
     print("{[}ENTRY 002{]} Efficiency increased by 347%")
