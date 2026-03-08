90488baebf39b7964519a0f2adfa0a59f4f6556d
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-08 21:52:44 +0000

    start the structure of the ex

--- python_05/ex0/stream_processor.py
+++ python_05/ex0/stream_processor.py
@@ -1,1 +1,13 @@
-start the ex
+from abc import ABC, abstractmethod
+from typing import Any, List
+
+def from_output():
+    pass
+
+def NumericProcessor():
+    pass
+
+def TextProcessor():
+    pass
+
+if __name__ == "__main__":
