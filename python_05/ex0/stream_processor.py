d3adddb353324e36ab4f78ab415c4f0769ff2b50
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-09 05:18:39 +0000

    finish step 2

--- python_05/ex0/stream_processor.py
+++ python_05/ex0/stream_processor.py
@@ -42,13 +42,40 @@ class NumericProcessor(DataProcessor):
 
         return self.format_output(result)
 
+
+class TextProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        return type(data) == str
+    
+    def process(self, data:Any) -> str:
+        if not self.validate(data):
+            return self.format_output("Error: Invalid text data")
+        
+        num_chars = len(data)
+        num_words = len(data.split())
+
+        result = (
+            f"Validation: Text data verified"
+            f"\nProcessed text: {num_chars} characters, {num_words} words"
+        )
+
+        return self.format_output(result)
+
+
 if __name__ == "__main__":
     print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===")
 
     print("\nInitializing Numeric Processor...")
 
     processor = NumericProcessor()
-    data = [1, 2, "a", 4, 5]
+    data = [1, 2, 3, 4, 5]
 
     print("Processing data:",data)
     print(processor.process(data))
+
+    print("\nInitializing Text Processor...")
+    processor = TextProcessor()
+    data = "Hello Nexus World"
+    print(processor.process(data))
+
+    print("\nInitializing Log Processor...")
