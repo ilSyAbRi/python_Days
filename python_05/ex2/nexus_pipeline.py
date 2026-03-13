12354adfb458652c9b92cb7febdab92b68695ca5
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-13 02:00:29 +0000

    bluid the structure of the ex

--- python_05/ex2/nexus_pipeline.py
+++ python_05/ex2/nexus_pipeline.py
@@ -1,6 +1,75 @@
 from abc import ABC, abstractmethod
 from typing import Any, Protocol, Union, List
 
+class ProcessingStage(Protocol):
+    def process(self, data: Any) -> Any:
+        ...
+
+
+class InputStage:
+    def process(self, data: Any) -> Any:
+        pass
+
+
+class TransformStage:
+    def process(self, data: Any) -> Any:
+        pass
+
+class OutputStage:
+    def process(self, data: Any) -> Any:
+        pass
+
+class ProcessingPipeline(ABC):
+
+    def __init__(self, pipeline_id: str):
+        self.pipeline_id = pipeline_id
+        self.stages: List[ProcessingStage] = []
+        self.stats = defaultdict(int)
+
+    def add_stage(self, stage: ProcessingStage):
+        pass
+
+    def run_stages(self, data: Any) -> Any:
+        pass
+
+    @abstractmethod
+    def process(self, data: Any) -> Union[str, Any]:
+        pass
+
+
+class JSONAdapter(ProcessingPipeline):
+
+    def process(self, data: Any) -> Union[str, Any]:
+        pass
+
+
+class CSVAdapter(ProcessingPipeline):
+
+    def process(self, data: Any) -> Union[str, Any]:
+        pass
+
+
+class StreamAdapter(ProcessingPipeline):
+
+    def process(self, data: Any) -> Union[str, Any]:
+        pass
+
+
+class NexusManager:
+
+    def __init__(self):
+        self.pipelines: List[ProcessingPipeline] = []
+
+    def register_pipeline(self, pipeline: ProcessingPipeline):
+        pass
+
+    def run_all(self, data_list: List[Any]):
+        pass
+
+    def chain(self, pipelines: List[ProcessingPipeline], data: Any):
+        pass
+
+
 if  __name__ == "__main__":
     print("=== CODE NEXUS - ENTERPRISE PIPELINE SYSTEM ===")
     print("Initializing Nexus Manager...")
