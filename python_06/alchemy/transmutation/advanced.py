312e4482fd2362873fa3473562cb23d21553d8fb
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-18 17:13:43 +0000

    do type hint

--- python_06/alchemy/transmutation/advanced.py
+++ python_06/alchemy/transmutation/advanced.py
@@ -2,11 +2,11 @@ from .basic import lead_to_gold
 from ..potions import healing_potion
 
 
-def philosophers_stone():
+def philosophers_stone() -> str:
     lead_result = lead_to_gold()
     heal_result = healing_potion()
     return f"Philosopher's stone created using {lead_result} and {heal_result}"
 
 
-def elixir_of_life():
+def elixir_of_life() -> str:
     return "Elixir of life: eternal youth achieved!"
