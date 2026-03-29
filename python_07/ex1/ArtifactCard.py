eb27296c954da5411bf1c1aec11e85590a7995a6
ilSyAbRi <iliasssyabri1@gmail.com>
2026-03-29 17:42:20 +0100

    finish two file

---
+++ python_07/ex1/ArtifactCard.py
@@ -0,0 +1,24 @@
+from ex0.Card import Card
+
+
+class ArtifactCard(Card):
+    def __init__(self, name: str, cost: int,
+                 rarity: str, durability: int, effect: str) -> None:
+        super().__init__(name, cost, rarity)
+        self.durability = durability
+        self.effect = effect
+
+    def play(self, game_state: dict) -> dict:
+        game_state["mana"] -= self.cost
+        return {
+            "card_played": self.name,
+            "mana_used": self.cost,
+            "effect": f"Permanent: {self.effect}"
+        }
+
+    def activate_ability(self) -> dict:
+        return {
+            "artifact": self.name,
+            "effect": self.effect,
+            "durability": self.durability
+        }
--- python_07/ex1/SpellCard.py
+++ python_07/ex1/SpellCard.py
@@ -2,6 +2,21 @@ from ex0.Card import Card
 
 
 class SpellCard(Card):
-    def __init__(self, name: str, cost: int, rarity: str, effect_type: str) -> None:
+    def __init__(self, name: str, cost: int, 
+                 rarity: str, effect_type: str) -> None:
         super().__init__(name, cost, rarity)
         self.effect_type = effect_type
+
+    def play(self, game_state: dict) -> dict:
+        return {
+            "card_played": self.name,
+            "mana_used": self.cost,
+            "effect": f"Deal {self.cost} {self.effect_type} to target"
+        }
+
+    def resolve_effect(self, targets: list) -> dict:
+        return {
+                "spell": self.name,
+                "effect_type": self.effect_type,
+                "targets": targets,
+        }
