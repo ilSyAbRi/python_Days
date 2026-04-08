2ef7e9a89e1899728a2ea62c4a3cf7fd445afbd5
ilSyAbRi <iliasssyabri1@gmail.com>
2026-04-08 16:15:59 +0100

    do battle fucntion

--- python_07/battle.py
+++ python_07/battle.py
@@ -11,3 +11,16 @@ def test_factory(factory):
 
     print(evolved.describe())
     print(evolved.attack())
+
+def battle(factory1, factory2):
+
+    base_creature1 = factory1.create_base()
+    base_creature2 = factory2.create_base()
+    
+    print(base_creature1.describe())
+    print("vs.")
+    print(base_creature2.describe())
+
+    print("fight!")
+    print(base_creature1.attack())
+    print(base_creature2.attack())
